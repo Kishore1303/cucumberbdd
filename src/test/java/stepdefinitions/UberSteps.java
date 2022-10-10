95004a9f83ae601b3f45ee9137ecd9cd22545de8
Kishore M V <kishoremv@Kishores-MacBook-Air.local>
2022-10-10 23:10:12 +0530

    second commit cucumber

--- src/test/java/stepdefinitions/UberSteps.java
+++ src/test/java/stepdefinitions/UberSteps.java
@@ -5,32 +5,30 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class UberSteps {
-	@Given("User wants to select carType {string} from uber app")
-	public void user_wants_to_select_car_type_from_uber_app(String string) {
+	@Given("User wants to select carType {string} from uber application")
+	public void user_wants_to_select_car_type_from_uber_app(String carType) {
 	    System.out.println();
 	}
 
 	@When("User select the car {string} and  from {string} and drop location {string}")
-	public void user_select_the_car_and_from_and_drop_location(String string, String string2, String string3) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	public void user_select_the_car_and_from_and_drop_location(String carType, String pickUp, String drop) {
+	   System.out.println("carType "+carType+" Pickup "+pickUp+" Drop "+drop);
 	}
-
+	
 	@Then("Driver starts the journey")
 	public void driver_starts_the_journey() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
 	}
 
 	@Then("Driver ends the journey")
 	public void driver_ends_the_journey() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
 	}
 
-	@Then("User pays Fare {string}")
-	public void user_pays_fare(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	@Then("User pays Fare {int}")
+	public void user_pays_fare(Integer fare) {
+		System.out.println("Fare "+fare);
+	}
+	
+	@Then("User checks trip details")
+	public void user_checks_trip_details() {
 	}
 }
--- target/test-classes/AppFeatures/Billing.feature
+++ target/test-classes/AppFeatures/Billing.feature
@@ -2,15 +2,15 @@ Feature: Calculate Billing amount
 
 Scenario Outline: Billing amount
 	Given User is on billing page
-	When user enters billing amount <billingamount>
-	When user enters tax amount <taxamount>
+	When user enters billing amount "<billingamount>"
+	When user enters tax amount "<taxamount>"
 	And user clicks on calculate button
-	Then it gives final amount <finalamount>
+	Then it gives final amount "<finalamount>"
 	
 		Examples:
 		| billingamount | taxamount | finalamount |
 		| 100						| 10				| 110					|
-		|	292						| 22				| 304					|
+		|	292						| 20				| 312					|
 		| 150						|	15.5			|	165.5				|
 		
 	
